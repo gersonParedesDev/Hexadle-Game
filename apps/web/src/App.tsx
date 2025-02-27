56b5e4ea46f5bc05d1d64a5f5f332700135de0c5
gersonUb <gersoncabj@gmail.com>
2025-02-27 02:21:24 -0300

    Start the Wordle game

---
+++ apps/web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div className='min-h-screen bg-black'>
+      <h1>hello wordl</h1>
+    </div>
+  )
+}
+
+export default App
