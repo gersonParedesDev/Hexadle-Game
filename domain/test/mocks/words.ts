0cb9bf740d24bfb3d15f3a6738c30b7f8276ddf9
gersonUb <gersoncabj@gmail.com>
2025-02-27 03:54:52 -0300

    Add test cases for Wordle logic validation

---
+++ domain/test/mocks/words.ts
@@ -0,0 +1,13 @@
+export const mockDictionary = new Set<string>([
+    'PERA',
+    'VELA',
+    'SAPO',
+    'TILO',
+  ]);
+
+export const mockValidWord = 'PERA';
+export const mockInvalidWord = 'XYZP';
+export const mockCorrectGuess = 'PERA';
+export const mockPartialGuess1 = 'SAPO';
+export const mockPartialGuess2 = 'VELA';
+export const mockIncorrectGuess = 'TILO';
