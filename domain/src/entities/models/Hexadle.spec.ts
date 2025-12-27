319f1ac5f8a986a0056692a68ed2a85947fd1729
gersonUb <gersoncabj@gmail.com>
2025-12-27 18:31:22 -0300

    fixes: add game status and hex comparator

--- domain/src/entities/models/Hexadle.spec.ts
+++ domain/src/entities/models/Hexadle.spec.ts
@@ -1,6 +1,7 @@
 import { beforeEach, describe, expect, test } from "vitest";
 import { HexCode } from "../value-objects/HexCode";
 import { Hexadle } from "./Hexadle";
+import { GameStatus } from "../value-objects/GameStatus";
 import { GameOverError } from "../../errors/GameOverError";
 import { HexFeedback } from "../value-objects/HexFeedback";
 
@@ -13,27 +14,50 @@ describe("Hexadle game", () => {
         game = new Hexadle(hexSecret);
     });
 
-    test("It should mark the game as solved if I guess the secret word", () => {
-        game.sendAttempt(new HexCode("A10")); 
-        expect(game.isSolved).toBe(true);
-    })
-
-    test("It should save the attempt in the history and deduct one life", () => {
+    test("deberia guardar el intento en el historial y descontar una vida", () => {
         const failedAttempt = new HexCode("BBBB")
         game.sendAttempt(failedAttempt);
-        expect(game.isSolved).toBe(false);
+        expect(game.status).toBe(GameStatus.PLAYING);
         expect(game.attemptsCount).toBe(1);
         expect(game.lives).toBe(4)
     })
 
-    test("It should return the correct feedback (colors) after an attempt", () => {
-        const attempt = new HexCode("AF0");
-        const resultado = game.sendAttempt(attempt);
+    test("deberia lanzar un error (Game Over) si intento jugar sin vidas", () => {
+        const failedAttempt = new HexCode("AAAAA");
 
+        for (let i = 0; i < 5; i++) {
+            game.sendAttempt(failedAttempt);
+        }
+        expect(game.lives).toBe(0);
+        expect(game.status).toBe(GameStatus.LOST);
+        expect(() => { game.sendAttempt(failedAttempt)}).toThrow(GameOverError);
+    });
+
+    test("It should return the correct feedback (colors) after an attempt", () => {
+        const secreto = new HexCode("A10"); 
+        const game = new Hexadle(secreto);
+        const attempt = new HexCode("AF0");
+
+        const resultado = game.sendAttempt(attempt);
         expect(resultado).toEqual([
             HexFeedback.CORRECT,
             HexFeedback.ABSENT,
             HexFeedback.CORRECT
         ]);
+        
+
+        expect(game.lives).toBe(4);
     });
+
+    test("deberia marcar el juego como GANADOR si el intento es igual al secreto", () => {
+        const secreto = new HexCode("C0FFEE");
+        const game = new Hexadle(secreto);
+        const intentoGanador = new HexCode("C0FFEE");
+
+        const feedback = game.sendAttempt(intentoGanador);
+
+        expect(game.status).toBe(GameStatus.WON);
+        expect(feedback.every(f => f === HexFeedback.CORRECT)).toBe(true);
+    });
+
 })
--- domain/src/entities/models/Hexadle.ts
+++ domain/src/entities/models/Hexadle.ts
@@ -1,36 +1,43 @@
 import { GameOverError } from "../../errors/GameOverError";
 import { HexComparator } from "../../services/HexComparator";
+import { GameStatus } from "../value-objects/GameStatus";
 import { HexCode } from "../value-objects/HexCode";
 import { HexFeedback } from "../value-objects/HexFeedback";
 
 
 export class Hexadle{
-    private _isSolved: boolean;
+    private _status: GameStatus;
     private static readonly MAX_LIVES = 5;
     private readonly hexSecret: HexCode;
     private attempts: HexCode[];
 
     constructor(secret: HexCode){;
-        this._isSolved = false;
+        this._status = GameStatus.PLAYING;
         this.hexSecret = secret;
         this.attempts = []
     }
 
-    get isSolved(): boolean { return this._isSolved; }
+    get status(): GameStatus { return this._status; }
     get attemptsCount(): number { return this.attempts.length; }
     get lives(): number { return Hexadle.MAX_LIVES - this.attempts.length; }
 
     sendAttempt(attempt: HexCode): HexFeedback[] {
-        const feedback = HexComparator.compare(this.hexSecret, attempt);
-
-        if (this.lives === 0) {
+        if (this._status !== GameStatus.PLAYING) {
             throw new GameOverError();
         }
-        if(this.hexSecret.equals(attempt)){
-            this._isSolved = true;
-        }
 
-        this.attempts.push(attempt);
+        const feedback = HexComparator.compare(this.hexSecret, attempt);
+
+        if (this.hexSecret.equals(attempt)) {
+            this._status = GameStatus.WON;
+        } else {
+            
+            this.attempts.push(attempt);
+            
+            if (this.lives === 0) {
+                this._status = GameStatus.LOST;
+            }
+        }
         return feedback;
     }
 
