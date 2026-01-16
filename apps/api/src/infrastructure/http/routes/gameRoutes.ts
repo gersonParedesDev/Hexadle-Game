872860d360fcf07b3a9d109f617683c008bcd0cd
gersonUb <gersoncabj@gmail.com>
2026-01-16 06:42:02 -0300

    feat(api): implement controller and routes"

--- apps/api/src/infrastructure/http/routes/gameRoutes.ts
+++ apps/api/src/infrastructure/http/routes/gameRoutes.ts
@@ -1,13 +1,17 @@
 import { FastifyInstance } from 'fastify';
 import { PostGameController } from '../controllers/PostGameController.js';
+import { PostAttemptController } from '../controllers/PostAttemptController.js';
 
 export async function gameRoutes(fastify: FastifyInstance) {
-
   fastify.post('/games', async (request, reply) => {
-
     const controller = new PostGameController();
     
     await controller.run(request, reply);
+  });
+
+  fastify.post('/games/:id/attempts', async (req, reply) => {
+    const controller = new PostAttemptController();
+    await controller.run(req, reply);
   });
   
 }
