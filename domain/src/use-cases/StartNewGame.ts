502d4d58c10359bd6ed82a5d9f87f9583edcc5eb
gersonUb <gersoncabj@gmail.com>
2026-01-20 14:46:01 -0300

    refactor: start new game return length and decimalValue

--- domain/src/use-cases/StartNewGame.ts
+++ domain/src/use-cases/StartNewGame.ts
@@ -3,19 +3,31 @@ import { randomUUID } from "crypto";
 import { GameRepository } from "../ports/GameRepository.js";
 import { SecretGenerator } from "../ports/SecretGenerator.js";
 
+export interface StartGameOutput {
+    id: string;
+    length: number;
+    decimalValue: string; 
+}
+
 export class StartNewGame {
     constructor(
         private readonly repository: GameRepository,
         private readonly secretGenerator: SecretGenerator
     ) {}
 
-    async execute(): Promise<string> {
+    async execute(): Promise<StartGameOutput> {
         const gameId = randomUUID();
         const secret = this.secretGenerator.generate();
+
+        const decimalValue = parseInt(secret.value, 16).toString();
 
         const newGame = new Hexadle(gameId, secret);
         await this.repository.save(newGame);
 
-        return newGame.id;
+        return {
+            id: newGame.id,
+            length: secret.value.length,
+            decimalValue: decimalValue
+        };
     }
 }
